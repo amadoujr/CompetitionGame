a5e58b663238db357563129a52936ab97fd82513
nathan.gerussi.etu <nathan.gerussi.etu@a10p21.fil.univ-lille.fr>
2022-09-27 14:33:06 +0200

    little update

---
+++ src/test/TournamentTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import competition.League;
+import competition.Tournament;
+import competitor.Competitor;
+import match.Match;
+import match.RandomVictoryMatch;
+import java.util.*;
+
+class TournamentTest {
+	
+	private Tournament tournament;
+	private List<Competitor> competitors;
+	private Match m1; 
+	private List<Competitor> qualify;
+
+	@BeforeEach
+	void setUp() throws Exception { 
+		this.competitors = new ArrayList<>();
+		this.m1 = new MockMatch();
+		this.tournament = new Tournament(competitors , m1);
+	}
+	
+	@Test
+	public void apresMatchTestOK() {
+		Competitor c1 = new Competitor("madrid");
+		Competitor c2 = new Competitor("barcelone");
+		this.competitors.add(c1);
+		this.competitors.add(c2);
+		this.qualify.addAll(this.competitors) ;
+		assertTrue(this.qualify.size() == 2);
+//		this.tournament.apresMatch(c1, c2);
+//		assertTrue(this.qualify.size() == 1);
+	}
+
+}
