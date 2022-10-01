fd02dfd075e283d37b235667227ea003c9891c1b
bbk hamza <mohamed.benboubker.etu@univ-lille.fr>
2022-10-01 20:05:47 +0200

    displayclass

---
+++ src/display/Display.java
@@ -0,0 +1,13 @@
+package display;
+
+public class Display {
+	private String msg;
+	
+	public Display(String msg) {
+		this.msg=msg;
+	}
+	public	void  displaymsg() {
+		System.out.println(msg);
+	}
+
+}
