a6361a5975cff6a6e7f8a83607d5db6a5686eff5
nathan.gerussi.etu <nathan.gerussi.etu@a16p7.fil.univ-lille.fr>
2022-10-21 16:30:30 +0200

    création interface partition et fonction globale

--- src/competition/Selection.java
+++ src/competition/Selection.java
@@ -7,7 +7,5 @@ public interface Selection {
 	
 	public void selectionType( List<Competitor> competitors);
 	
-	public List<List<Competitor>> doPartition(List<Competitor> competitors);
-	
 	public List<Competitor> getFinalist(List<List<Competitor>> qualified);
 }
---
+++ src/partition/Partition.java
@@ -0,0 +1,7 @@
+package partition;
+import competitor.* ;
+import java.util.* ;
+
+public interface Partition {
+	public List<List<Competitor>> doPartition ( List<Competitor> competitors) ;
+}
