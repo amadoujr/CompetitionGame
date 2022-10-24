95f09bcaaa2fba3b7e2ced74d69298902e5f014d
amadouu <amadouu@localhost>
2022-10-24 21:15:13 +0200

    add tests class

---
+++ src/competition/SelectionTwo.java
@@ -0,0 +1,17 @@
+package competition;
+
+import java.util.List;
+
+import competitor.Competitor;
+
+public class SelectionTwo implements Selection {
+
+	
+
+	@Override
+	public List<Competitor> getFinalist(List<League> qualified) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
