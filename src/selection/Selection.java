afd68a44eb518c068ceb57702da5da5bf466efc8
barro <amadou.barro.etu@univ-lille.fr>
2022-11-25 20:08:37 +0100

    latest modification

--- src/selection/Selection.java
+++ src/selection/Selection.java
@@ -8,7 +8,7 @@ public interface Selection {
 	
 	/**
 	 * this methods return competitors who's reach the final stage
-	 * @param qualified 
+	 * @param qualified  
 	 * @return
 	 */
 	public List<Competitor> getFinalist(List<League> qualified);
